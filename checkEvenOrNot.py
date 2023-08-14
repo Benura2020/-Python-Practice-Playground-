d175ef1a73606af6ceba7db61aff6d9711f5cf17
Benura Wickramanayake <70945510+Benura2020@users.noreply.github.com>
2023-08-14 22:23:14 +0530

    Add files via upload

---
+++ checkEvenOrNot.py
@@ -0,0 +1,25 @@
+"""
+
+             /\_/\
+            (  ･ω･)
+|------------U-----U----------|
+|       MMXXIII_VIII_XIV      |
+|Check the numbers in list "a"|
+|       are Even or Not       |
+| by using List Comprehension |
+|_____________________________|
+            |     |
+            |     |
+             U   U
+
+"""
+
+a = [10, 13, 15, 16, 18, 29]
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+b = [is_even(i) for i in a]
+print(b)
